f509b606e2696a1e5ed28e0c9861f37d71884ae7
Ngoc Khanh <khanhk56a12@gmail.com>
2023-05-29 12:46:09 +0700

    init project

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  const HeaderContainer = styled.div``;
+  const HeaderTop = styled.div``;
+  const HeaderMiddle = styled.div``;
+  const HeaderBottom = styled.div``;
+
+  return <HeaderContainer></HeaderContainer>;
+};
+
+export default Header;
